e9c9d8fe465da01ad3c292ecbc7954aec9a66a53
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-04 13:46:42 -0700

    Add form and UI logic to process form submit

--- src/main.js
+++ src/main.js
@@ -2,3 +2,16 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Calculator from './calc.js'
+
+$(document).ready(function(){
+  // Calculate Click Event Handler
+  $("#calculateBtn").click(function(){
+    $(".calculateDiv").hide();
+    $(".formDiv").show();
+    // Form Submit Event Handler
+    $("form").submit(function(event){
+      event.preventDefault();
+    })
+  });
+});
