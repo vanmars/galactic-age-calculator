1d0c84bf0670a77f63c3a2fd01aeb9fe2e718519
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-04 09:35:46 -0700

    Pass TEST 1 and correct .babelrc file info

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -5,8 +5,9 @@ describe('Calculator', () => {
   beforeEach(() => {
     newCalc = new Calculator(31, 79);
   });
-  test('should store inputted values of age and life expectancy', () => {
-      expect(newCalc.age).toEqual(31);
-      expect(newCalc.lifeExpectancy).toEqual(79);
+test('should store inputted values of age and life expectancy', () => {
+    // const newCalc = new Calculator(31, 79);
+    expect(newCalc.age).toEqual(31);
+    expect(newCalc.lifeExpectancy).toEqual(79);
   });
 });
--- src/calc.js
+++ src/calc.js
@@ -7,5 +7,7 @@
 
 export default class Calculator {
   constructor (age, lifeExpectancy){
+    this.age = age;
+    this.lifeExpectancy = lifeExpectancy;
   };
 };
