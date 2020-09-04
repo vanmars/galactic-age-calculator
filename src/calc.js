d513fb85281c492030bed276093c179ad0004da2
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-04 10:06:22 -0700

    PASS Test 5: Calculate Jupiter Age

--- src/calc.js
+++ src/calc.js
@@ -12,6 +12,6 @@ export default class Calculator {
     this.mercuryAge = Math.floor(this.age/.24);
     this.venusAge = Math.floor(this.age/.62);
     this.marsAge = Math.floor(this.age/1.88);
-    this.jupiterAge;
+    this.jupiterAge = Math.floor(this.age/11.86);
   };
 };
