8ba1d666dfd7c904c2722eac4c29e147f4ed4c73
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-04 14:15:10 -0700

    Add resultDiv and use template literal to populate results of calculations to screen

--- src/main.js
+++ src/main.js
@@ -12,6 +12,48 @@ $(document).ready(function(){
     // Form Submit Event Handler
     $("form").submit(function(event){
       event.preventDefault();
-    })
+      const inputtedAge = parseInt($("#age").val());
+      const inputtedLifeExpectancy = parseInt($("#lifeExpectancy").val());
+      let newCalc = new Calculator(inputtedAge, inputtedLifeExpectancy);
+      $(".formDiv").hide();
+      $(".resultDiv").show();
+      if(newCalc.age <= newCalc.lifeExpectancy){
+        $(".resultDiv").html(
+          `<p class='lead'>You are . . .</p>
+          <ul>
+            <li>${newCalc.mercuryAge} Mercurian years old</li>
+            <li>${newCalc.venusAge} Venusian years old</li>
+            <li>${newCalc.marsAge} Martian years old</li>
+            <li>${newCalc.jupiterAge} Jovian years old</li>
+          </ul
+          <hr>
+          <p class='lead'>You have . . .</p>
+          <ul>
+            <li>${newCalc.mercuryYearsLeftOrPast} Mercurian years left</li>
+            <li>${newCalc.venusYearsLeftOrPast} Venusian years left</li>
+            <li>${newCalc.marsYearsLeftOrPast} Martian years left</li>
+            <li>${newCalc.jupiterYearsLeftOrPast} Jovian years left</li>
+          </ul>`
+        );
+      } else {
+        $(".resultDiv").html(
+          `<p class='lead'>You are . . .</p>
+          <ul>
+            <li>${newCalc.mercuryAge} Mercurian years old</li>
+            <li>${newCalc.venusAge} Venusian years old</li>
+            <li>${newCalc.marsAge} Martian years old</li>
+            <li>${newCalc.jupiterAge} Jovian years old</li>
+          </ul
+          <hr>
+          <p class='lead'>You have lived . . .</p>
+          <ul>
+            <li>${newCalc.mercuryYearsLeftOrPast} Mercurian years past expectancy</li>
+            <li>${newCalc.venusYearsLeftOrPast} Venusian years past expectancy</li>
+            <li>${newCalc.marsYearsLeftOrPast} Martian years past expectancy</li>
+            <li>${newCalc.jupiterYearsLeftOrPast} Jovian years past expectancy</li>
+          </ul>`
+        );
+      }
+    });
   });
 });
