2df08ce687e5736f1ebc23607b3260570c572c00
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-04 16:37:20 -0700

    Add image files to fill out planet card grid

--- src/main.js
+++ src/main.js
@@ -20,55 +20,55 @@ $(document).ready(function(){
       if(newCalc.age <= newCalc.lifeExpectancy){
         $("#mercuryResult").html(
           `<ul>
-             <li>You are ${newCalc.mercuryAge} Mercurian years old</li></ul>
+             <li>You are ${newCalc.mercuryAge} Mercurian years old</li>
              <li>You have ${newCalc.mercuryYearsLeftOrPast} Mercurian years left</li>
           </ul>`
         );
         $("#venusResult").html(
           `<ul>
-             <li>You are ${newCalc.venusAge} Venusian years old</li></ul>
+             <li>You are ${newCalc.venusAge} Venusian years old</li>
              <li>You have ${newCalc.venusYearsLeftOrPast} Venusian years left</li>
           </ul>`
         );
         $("#marsResult").html(
           `<ul>
-             <li>You are ${newCalc.marsAge} Martian years old</li></ul>
+             <li>You are ${newCalc.marsAge} Martian years old</li>
              <li>You have ${newCalc.marsYearsLeftOrPast} Martian years left</li>
           </ul>`
         );
         $("#earthResult").html(
           `<ul>
-             <li>You are ${newCalc.age} Earth years old</li></ul>
+             <li>You are ${newCalc.age} Earth years old</li>
              <li>You have ${newCalc.earthYearsLeftOrPast} Earth years left</li>
           </ul>`
         );
         $("#jupiterResult").html(
           `<ul>
-             <li>You are ${newCalc.jupiterAge} Jovian years old</li></ul>
+             <li>You are ${newCalc.jupiterAge} Jovian years old</li>
              <li>You have ${newCalc.jupiterYearsLeftOrPast} Joavian years left</li>
           </ul>`
         );
         $("#saturnResult").html(
           `<ul>
-             <li>You are ${newCalc.saturnAge} Saturnian years old</li></ul>
+             <li>You are ${newCalc.saturnAge} Saturnian years old</li>
              <li>You have ${newCalc.saturnYearsLeftOrPast} Saturnian years left</li>
           </ul>`
         );
         $("#uranusResult").html(
           `<ul>
-             <li>You are ${newCalc.uranusAge} Uranian years old</li></ul>
+             <li>You are ${newCalc.uranusAge} Uranian years old</li>
              <li>You have ${newCalc.uranusYearsLeftOrPast} Uranian years left</li>
           </ul>`
         );
         $("#neptuneResult").html(
           `<ul>
-             <li>You are ${newCalc.neptuneAge} Neptunian years old</li></ul>
+             <li>You are ${newCalc.neptuneAge} Neptunian years old</li>
              <li>You have ${newCalc.neptuneYearsLeftOrPast} Neptunian years left</li>
           </ul>`
         );
         $("#plutoResult").html(
           `<ul>
-             <li>You are ${newCalc.plutoAge} Plutonian years old</li></ul>
+             <li>You are ${newCalc.plutoAge} Plutonian years old</li>
              <li>You have ${newCalc.plutoYearsLeftOrPast} Plutonian years left</li>
           </ul>`
         );
@@ -100,55 +100,55 @@ $(document).ready(function(){
       } else {
         $("#mercuryResult").html(
           `<ul>
-             <li>You are ${newCalc.mercuryAge} Mercurian years old</li></ul>
+             <li>You are ${newCalc.mercuryAge} Mercurian years old</li>
              <li>You have lived ${newCalc.mercuryYearsLeftOrPast} Mercurian years past expectancy</li>
           </ul>`
         );
         $("#venusResult").html(
           `<ul>
-             <li>You are ${newCalc.venusAge} Venusian years old</li></ul>
+             <li>You are ${newCalc.venusAge} Venusian years old</li>
              <li>You have lived  ${newCalc.venusYearsLeftOrPast} Venusian years past expectancy</li>
           </ul>`
         );
         $("#marsResult").html(
           `<ul>
-             <li>You are ${newCalc.marsAge} Martian years old</li></ul>
+             <li>You are ${newCalc.marsAge} Martian years old</li>
              <li>You have lived  ${newCalc.marsYearsLeftOrPast} Martian years past expectancy</li>
           </ul>`
         );
         $("#earthResult").html(
           `<ul>
-             <li>You are ${newCalc.age} Earth years old</li></ul>
+             <li>You are ${newCalc.age} Earth years old</li>
              <li>You have lived  ${newCalc.earthYearsLeftOrPast} Earth years past expectancy</li>
           </ul>`
         );
         $("#jupiterResult").html(
           `<ul>
-             <li>You are ${newCalc.jupiterAge} Jovian years old</li></ul>
+             <li>You are ${newCalc.jupiterAge} Jovian years old</li>
              <li>You have lived  ${newCalc.jupiterYearsLeftOrPast} Joavian years past expectancy</li>
           </ul>`
         );
         $("#saturnResult").html(
           `<ul>
-             <li>You are ${newCalc.saturnAge} Saturnian years old</li></ul>
+             <li>You are ${newCalc.saturnAge} Saturnian years old</li>
              <li>You have lived  ${newCalc.saturnYearsLeftOrPast} Saturnian years past expectancy</li>
           </ul>`
         );
         $("#uranusResult").html(
           `<ul>
-             <li>You are ${newCalc.uranusAge} Uranian years old</li></ul>
+             <li>You are ${newCalc.uranusAge} Uranian years old</li>
              <li>You have lived  ${newCalc.uranusYearsLeftOrPast} Uranian years past expectancy</li>
           </ul>`
         );
         $("#neptuneResult").html(
           `<ul>
-             <li>You are ${newCalc.neptuneAge} Neptunian years old</li></ul>
+             <li>You are ${newCalc.neptuneAge} Neptunian years old</li>
              <li>You have lived  ${newCalc.neptuneYearsLeftOrPast} Neptunian years past expectancy</li>
           </ul>`
         );
         $("#plutoResult").html(
           `<ul>
-             <li>You are ${newCalc.plutoAge} Plutonian years old</li></ul>
+             <li>You are ${newCalc.plutoAge} Plutonian years old</li>
              <li>You have lived  ${newCalc.plutoYearsLeftOrPast} Plutonian years past expectancy</li>
           </ul>`
 
