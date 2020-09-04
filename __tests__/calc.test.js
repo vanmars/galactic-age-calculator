38a85e7923b3272c69601092974a0cc0a430f5c9
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-04 10:14:53 -0700

    PASS Test 6: Calculate years left on each planet

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -21,7 +21,7 @@ test('4: should correctly return user age in mars years', () => {
 test('5: should correctly return user age in jupiter years', () => {
     expect(newCalc.jupiterAge).toEqual(2);
   });
-  test('6: should correctly return expected years left on each planet', () => {
+test('6: should correctly return expected years left on each planet', () => {
     expect(newCalc.mercuryYearsLeft).toEqual(200);
     expect(newCalc.venusYearsLeft).toEqual(77);
     expect(newCalc.earthYearsLeft).toEqual(48);
--- src/calc.js
+++ src/calc.js
@@ -13,10 +13,10 @@ export default class Calculator {
     this.venusAge = Math.floor(this.age/.62);
     this.marsAge = Math.floor(this.age/1.88);
     this.jupiterAge = Math.floor(this.age/11.86);
-    this.mercuryYearsLeft;
-    this.venusYearsLeft;
-    this.earthYearsLeft;
-    this.marsYearsLeft;
-    this.jupiterYearsLeft;
+    this.mercuryYearsLeft = Math.floor((this.lifeExpectancy - age)/.24);
+    this.venusYearsLeft = Math.floor((this.lifeExpectancy - age)/.62);
+    this.earthYearsLeft = Math.floor((this.lifeExpectancy - age));
+    this.marsYearsLeft = Math.floor((this.lifeExpectancy - age)/1.88);
+    this.jupiterYearsLeft = Math.floor((this.lifeExpectancy - age)/11.86);
   };
 };
